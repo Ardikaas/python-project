ac0aabd6a5b9d46cc24473107e7fcaad79895153
Ardikaas <ardikaajisetiawan@gmail.com>
2023-02-20 19:16:41 +0700

    membuat aplikasi kalkulator sederhana

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
-print("helllo world")
+print("Selamat datang di program kalkulator sederhana")
+
+x = int(input("masukkan bilangan pertama : "))
+operasi = str(input("masukkan operasi aritmatika : "))
+y = int(input("masukkan bilangan kedua : "))
+
+if operasi == "+":
+  hasil = x + y
+  print("operasi yang anda pilih adalah penjumlahan")
+  print(x, "+", y, "=", hasil)
+else:
+  print("operasi yang anda masukkan salah!!!")
